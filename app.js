2c828c3a8a447267d056ecc66611e368595a330a
Ismail Feham <ismail.feham64@gmail.com>
2022-05-19 01:26:13 -0400

    Mongoose connection established + React module added

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
 const express = require('express')
+require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000;
+const mongoose = require('mongoose');
+const uri = process.env.ATLAS_URI
+
+const Schema = mongoose.Schema;
+
+const itemSchema = new Schema({
+    name: {
+        type: String,
+        require: true,
+        minlength: 3
+    },
+}, {
+    timestamps: true,
+})
+
+mongoose.connect(uri, { useNewUrlParser: true})
+
+mongoose.connection.once('open', () => {
+    console.log('MongoDB connection: successfully opened')
+})
+app.use(express.json())
 
 app.listen(port,  () => {
     console.log("Server open on port " + port);
